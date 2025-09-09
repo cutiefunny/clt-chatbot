51f1aefe9b79003df80e054c765c86b05817a722
cutiefunny <cutiefunny@gmail.com>
2025-09-09 13:52:37 +0900

    시나리오 채팅창 레이아웃 변경

--- app/layout.js
+++ app/layout.js
@@ -20,9 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* 👇 폰트 클래스와 기본 클래스를 body에 직접 적용합니다. */}
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {/* 👇 ThemeApplier는 이제 children만 감싸는 역할만 합니다. */}
         <ThemeApplier>
           {children}
         </ThemeApplier>
