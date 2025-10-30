bb1370ecd8e1bf390960bc4a64b89e6863ea1da2
SulfurBottom <397love@gmail.com>
2025-10-30 17:45:06 +0900

    Implement toggle functionality for scenario panel with collapse icon. Added ScenarioCollapseIcon to replace ScenarioExpandIcon based on panel state.

---
+++ app/components/icons/ScenarioCollapseIcon.jsx
@@ -0,0 +1,20 @@
+export default function ScenarioCollapseIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+    >
+      <path
+        d="M8.72942 11.3096L8.72942 17.6129L7.02058 17.6129L7.02058 13.0184L2.42612 13.0184V11.3096L8.72942 11.3096Z"
+        fill="#5E7599"
+      />
+      <path
+        d="M17.5735 8.77413L17.5735 7.06529H12.9791L12.9791 2.47083H11.2702V8.77413H17.5735Z"
+        fill="#5E7599"
+      />
+    </svg>
+  );
+}
